5b5c5b817d537e1bdd071d9270a6b9c07c3967ce
Lady Stock <nikki.castlea@gmail.com>
2020-07-26 11:58:24 -0600

    api routes written

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,19 @@
+//Global Variables
+const savedNotes = require("../db/db.json");
+
+
+
 // TODO: GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
+app.get("/api/notes", (req, res) => {
+    res.json(savedNotes);
+})
+
 // TODO: POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+app.post("/api/notes", (req,res) => {
+    savedNotes.push(req.body);
+    res.json();
+})
 
 // TODO: DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,19 @@ const path = require("path");
 
 // TODO: The following HTML routes should be created:
     //GET `/notes` - Should return the `notes.html` file.
-    app.get("/notes", (req, res) => {
-        res.sendFile(path.join(__dirname,"../public/notes.html"));
-    })
+   
+   
+   module.exports = (app) => {
+       app.get("/notes", (req, res) => {
+            res.sendFile(path.join(__dirname,"../public/notes.html"));
+        })
+    
+        //GET `*` - Should return the `index.html` file
+    
+        app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
 
-    //GET `*` - Should return the `index.html` file
+   };
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    })
+       
